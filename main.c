9685c346282db62c995f48ea15dcc50999917f90
Craig Wilkinson <curlywurlycraig@gmail.com>
2021-01-13 08:49:39 +0000

    Implement memcpy and some matrix math

--- main.c
+++ main.c
@@ -1,42 +1,28 @@
+#include "mem.c"
+#include "math.c"
+
 // JS imports
 void prints(const char *address);
 void printfl(float num);
 void glClear(float r, float g, float b, float a);
 void drawVertexBuffer(float* vertices, unsigned int vertexCount);
+void setModelViewMatrix(float* matrix);
 void setTranslation(float x, float y);
 float random();
 
-// Create a perspective matrix
-// TODO
-void perspective(
-        float** result,
-        float fieldOfView,
-        float aspect,
-        float zNear,
-        float zFar
-) {
-    
-}
-
-// Why? The compiler occasionally will optimize large inline arrays
-// by placing them in .data (?), then memcpy-ing them out, or something.
-// There doesn't appear to be any way around this:
-//     https://www.raspberrypi.org/forums/viewtopic.php?t=219687
-char* memcpy(char* dest, const char* src, unsigned int n) {
-    for (int i = 0; i < n; i++) {
-        *(dest+i) = *(src+i);
-    }
-
-    return dest;
-}
+float* vertices = 0;
+static unsigned int MAX_VERTICES = 1000;
 
 void iter() {
-    const float vertices[] = {
-        -1 + 2 * random(), -1 + 2 * random(), -1 + 2 * random(),
-        -1 + 2 * random(), -1 + 2 * random(), -1 + 2 * random(),
-        -1 + 2 * random(), -1 + 2 * random(), -1 + 2 * random(),
-        -1 + 2 * random(), -1 + 2 * random(), -1 + 2 * random(),
+    const float newVertices[] = {
+        random() * 1.0f, random() * 1.0f,
+        random() * 1.0f, random() * 1.0f,
+        random() * 1.0f, random() * 1.0f,
+        random() * 1.0f, random() * 1.0f,
+        random() * -1.0f, random() * 1.0f,
     };
 
-    drawVertexBuffer((float*) vertices, 4);
+    memcpy(vertices, newVertices, sizeof(float) * 10);
+
+    drawVertexBuffer(vertices, 5);
 }
---
+++ math.c
@@ -0,0 +1,27 @@
+// Converts from clip space (-1 to 1) to pixel space
+void projection(
+        float* result,
+        float width,
+        float height 
+) {
+    float newResult[] = {
+        2 / width, 0, 0,
+        0, -2 / height, 0,
+        -1, 1, 1
+    };
+
+    memcpy(result, newResult, sizeof(float) * 9);
+}
+
+// Create a perspective matrix
+// TODO
+void perspective(
+        float** result,
+        float fieldOfView,
+        float aspect,
+        float zNear,
+        float zFar
+) {
+    
+}
+
